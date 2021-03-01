c769859ca229cec929ed7a73c29e5e63c9ac272d
Fabio Pinatti <fpinatti@gmail.com>
2021-03-01 12:16:36 -0300

    build tweaks

--- sites/ibn/wp-content/themes/pinapp/assets-src/postbuild.js
+++ sites/ibn/wp-content/themes/pinapp/assets-src/postbuild.js
@@ -1,15 +1,15 @@
-const fs = require('fs');
-const path = require('path');
-const assetsDir = '../assets/css';
-// Loop through the baseDir directory
-fs.readdir(assetsDir, (err, files) => {
-  files.forEach(file => {
-    const filext = path.extname(file);
-    if (filext === '.png' || 
-      filext === '.jpg' || 
-      filext === '.svg' || 
-      filext === '.gif') {
-        fs.unlinkSync(`${assetsDir}/${file}`);
-      }
-  });
-});
+// const fs = require('fs');
+// const path = require('path');
+// const assetsDir = '../assets/css';
+// // Loop through the baseDir directory
+// fs.readdir(assetsDir, (err, files) => {
+//   files.forEach(file => {
+//     const filext = path.extname(file);
+//     if (filext === '.png' || 
+//       filext === '.jpg' || 
+//       filext === '.svg' || 
+//       filext === '.gif') {
+//         fs.unlinkSync(`${assetsDir}/${file}`);
+//       }
+//   });
+// });
